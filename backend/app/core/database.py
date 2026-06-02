82e326ce372fe20010712e8b22273ed807242171
Kisanlink <mani.m@kisanlink.in>
2026-06-02 22:46:23 +0530

    Add Database connection setup with SQLAlchemey async support including a base model and session management

---
+++ backend/app/core/database.py
@@ -0,0 +1,23 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine,async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+class Base(DeclarativeBase): #Base lives here for now. Some teams move it to models/base.py later — either is fine.
+    pass
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=settings.app_env == "development",
+    )
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    )
+
+async def get_db() -> AsyncGenerator[AsyncSession,None]: #AsyncGenerator[YieldType, SendType]
+    async with AsyncSessionLocal() as session: #AsyncSessionLocal is a context manager that returns an AsyncSession object
+        yield session
