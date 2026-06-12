d6f039548c3f07bb3c94e049f17f7f88625ff1c0
Kisanlink <mani.m@kisanlink.in>
2026-06-12 11:05:56 +0530

    Add authentication schemas using Pydantic for user registration, login, and token management

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr, Field
+from sqlalchemy import desc
+
+class RegisterRequest(BaseModel):
+    email : EmailStr
+    password : str = Field(min_length=8,max_length=8,description="Password must be at least 8 characters long")
+
+class LoginRequest(BaseModel):
+    email : EmailStr
+    password : str 
+
+class RefreshRequest(BaseModel):
+    refresh_token : str = Field(description="Refresh token")
+
+class TokenResponse(BaseModel):
+    access_token : str = Field(description="Access token")
+    refresh_token : str = Field(description="Refresh token")
+    token_type : str = Field(description="Token type")
+
+class UserResponse(BaseModel):
+    id:str
+    email:EmailStr
+    is_verified:bool
+
+    model_config = {
+        "from_attributes": True,
+    }
